476f87899e5531b0ff21e7a0eb0577f14135a0a0
Ben Lambert <ben@bignoyeslab.com>
2023-02-13 14:20:57 -0500

    first commit

---
+++ web_app/app.py
@@ -0,0 +1,31 @@
+import os
+import db
+from flask import Flask, render_template
+
+###############################################################################
+# Global Configuration
+#
+# WSGI app wrapper
+app = Flask(__name__)
+# Database engine wrapper
+dat = db.Datastore(
+    os.getenv('DATABASE_USER'),
+    os.getenv('DATABASE_PASS'),
+    os.getenv('DATABASE_HOST'),
+    os.getenv('DATABASE_NAME'),
+)
+
+
+@app.route('/')
+def index():
+    return 'Welcome to Overshare! Yet another social media app.  Click <a href="/timeline">here</a> to view the timeline.'
+
+@app.route('/timeline')
+def timeline():
+    try:
+        return render_template('index.html', records=db.get_timeline(dat))
+    except:
+        return 'Is the database up and running?', 500
+
+if __name__ == '__main__':
+    db.populate_db(dat)
---
+++ web_app/db.py
@@ -0,0 +1,46 @@
+from faker import Faker
+from sqlalchemy import create_engine, text, select
+
+class Datastore:
+    def __init__(self, username, password, hostname, dbname):
+        self.engine = create_engine(
+            f"postgresql+psycopg://{username}:{password}@{hostname}/{dbname}",
+            echo=True, 
+            future=True
+        )
+
+    @property
+    def connection(self):
+        return self.engine.connect()
+
+
+def init_db(datastore: Datastore):
+    with datastore.connection as conn:
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS posts (
+                id          INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+                ,title       TEXT
+                ,username    TEXT 
+                ,content     TEXT
+                ,created_at  TIMESTAMP NOT NULL DEFAULT clock_timestamp()::timestamp
+            );
+        """))
+
+        conn.commit()
+
+    
+def populate_db(datastore: Datastore):    
+    with datastore.connection as conn:
+        fake = Faker(['ja_JP']) 
+        conn.execute(
+            text("INSERT INTO posts (title, username, content) VALUES (:title, :username, :content);"),
+            [{ 'title': fake.sentence(), 'username': fake.name(), 'content': fake.text()} for _ in range(100)]
+        )
+        conn.commit()
+
+
+def get_timeline(datastore: Datastore):    
+    with datastore.connection as conn:
+        return conn.execute(
+            text("SELECT title, username, content, created_at FROM posts LIMIT 100;")
+        ).all()
